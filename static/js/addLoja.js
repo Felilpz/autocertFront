ef120a53bedcbababe987e61ee1a998558e2b42f
Felilpz <luizfelipe.vieira12@gmail.com>
2025-03-05 22:31:54 -0300

    funcionality in cnpj and telefone added (showing left characters)

--- static/js/addLoja.js
+++ static/js/addLoja.js
@@ -1,5 +1,4 @@
 document.getElementById('add-new-pharmacy').addEventListener('click', function () {
-    console.log('funcionando click')
     const cnpj = document.getElementById('add-cnpj-modal').value.trim();
     const razaoSocial = document.getElementById('add-razao-modal').value.trim();
     const bandeira = document.getElementById('add-flag-modal').value.trim();
@@ -43,7 +42,45 @@ document.getElementById('add-new-pharmacy').addEventListener('click', function (
         return;
     }
 
-    console.log('dados enviados:', { cnpj, razaoSocial, bandeira, responsavel, telefone, email });
-    alert('loja adicionada c sucesso!');
-    document.getElementById('pharmacy-form').reset()
-})
+    const newPharmacy = {
+        cnpj,
+        razaoSocial,
+        bandeira,
+        responsavel,
+        telefone,
+        email,
+    };
+    console.log('dados enviados:', newPharmacy);
+
+    alert('Dados salvos com sucesso!');
+
+    document.getElementById('pharmacy-form').reset();
+});
+
+function limitarCaracteres(input, maxLength, mensagemElement) {
+    input.addEventListener('input', function () {
+        const valor = input.value.trim();
+        if (valor.length > maxLength) {
+            input.value = valor.slice(0, maxLength);
+        }
+        const caracteresRestantes = maxLength - input.value.length;
+        mensagemElement.textContent = `Caracteres restantes: ${caracteresRestantes}`;
+    });
+}
+
+// msg para CNPJ
+const cnpjInput = document.getElementById('add-cnpj-modal');
+const cnpjMensagem = document.createElement('div');
+cnpjMensagem.style.color = 'red';
+cnpjMensagem.style.fontSize = '12px';
+// cnpjMensagem.style.paddingRight = '50px' ver o pq n ta funcionando...
+cnpjInput.insertAdjacentElement('afterend', cnpjMensagem);
+limitarCaracteres(cnpjInput, 14, cnpjMensagem);
+
+// msg para telefone
+const telefoneInput = document.getElementById('add-cellphone-number-modal');
+const telefoneMensagem = document.createElement('div');
+telefoneMensagem.style.color = 'red';
+telefoneMensagem.style.fontSize = '12px';
+telefoneInput.insertAdjacentElement('afterend', telefoneMensagem);
+limitarCaracteres(telefoneInput, 11, telefoneMensagem);
