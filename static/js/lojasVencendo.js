55d662073559a83316f79dd5aedcb76c1198cd43
Felilpz <luizfelipe.vieira12@gmail.com>
2025-03-06 08:08:04 -0300

    wip

--- static/js/lojasVencendo.js
+++ static/js/lojasVencendo.js
@@ -1,3 +1,8 @@
-document.getElementById('lojasVencendo').addEventListener('click', function {
+document.getElementById('lojasVencendo').addEventListener('click', function () {
     console.log('click funcionando LOJASVENCENDO')
+
+    let i;
+    for (i = 0; i < 9; i++) {
+        console.log(i)
+    }
 })
