b9567567e81a338cb17b692533bd4c311c331b85
Felilpz <luizfelipe.vieira12@gmail.com>
2025-03-06 07:47:33 -0300

    selected filters going to console

--- static/js/edit.js
+++ static/js/edit.js
@@ -39,3 +39,25 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+document.getElementById('confirm-filters').addEventListener('click', function () {
+    console.log('click filtro funcionando')
+
+    let flagSelected = document.getElementById('filter-by-flag').value.trim()
+    let ownerSelected = document.getElementById('filter-owner').value.trim()
+
+    const filters = {
+        flagSelected,
+        ownerSelected
+    }
+
+    console.log('filtros selecionados:', filters)
+    document.getElementById('filter-form').reset();
+
+    // let modal = document.getElementById('filterModal')
+    // modal.style.display = 'none'
+
+    let modal = bootstrap.Modal.getInstance(document.getElementById('filterModal'));
+    modal.hide();
+})
