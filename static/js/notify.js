5f1485ae66edda41d6cce9f3474e1cf1b5d3ee88
Felilpz <luizfelipe.vieira12@gmail.com>
2025-03-07 22:42:10 -0300

    notifiy engine done

--- static/js/notify.js
+++ static/js/notify.js
@@ -1,10 +1,34 @@
 document.getElementById('notify').addEventListener('click', function () {
     console.log('Notificacao clicada')
 
-    const confirmation = window.confirm('Voce deseja fazer o envio manual da notificação para *** ?')
-    if (confirmation) {
-        console.log('Notificacao enviada com sucesso')
-    } else {
-        console.log('Operacao cancelada')
+    // let responsavelPharmacy = 'Maicon Santana';
+    let name = window.prompt('Digite seu nome: ')
+    // const confirmation = window.confirm(`${name}, voce deseja fazer o envio manual da notificação para ${responsavelPharmacy} ?`)
+    // if (confirmation) {
+    //     console.log('Notificacao enviada com sucesso')
+    // } else {
+    //     console.log('Operacao cancelada')
+    // }
+
+    let days = document.querySelector('.dias-para-vencer p').textContent;
+    // console.log(`${days}`)
+    if (days >= 15) {
+        const notificacaoConfirm = confirm(`${name}, deseja notificar a validade do certificado a certificaminas?`)
+        if (notificacaoConfirm) {
+            console.log('Notificacao enviada no grupo do whatsapp!')
+        } else {
+            console.log('Operacao cancelada')
+        }
+    }
+
+    if (days <= 14) {
+        const notificacaoConfirm = confirm(`${name}, deseja notificar o associado responsavel atraves do whatsapp e email?`)
+
+        if (notificacaoConfirm) {
+            console.log('Notificacao enviada no grupo do whatsapp!')
+            console.log('Notificacao enviada para o email!')
+        } else {
+            console.log('Operacao cancelada')
+        }
     }
 })
