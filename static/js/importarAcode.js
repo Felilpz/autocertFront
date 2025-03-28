79b1beaebe6b6e039c242da7e502ec10c8079b92
Felilpz <luizfelipe.vieira12@gmail.com>
2025-03-28 13:14:23 -0300

    import acode redirect

---
+++ static/js/importarAcode.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const importarACODE = document.getElementById('importarACODE');
+    importarACODE.addEventListener('click', function () {
+        window.open('https://acode.com.br/termos/redefarmes', '_blank');
+    });
+});
